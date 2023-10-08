46bee1ec8f6ae0a7b0855880e31c5c367e120b12
keet7 <keerthana.pepeti@gmail.com>
2023-10-08 20:53:37 +0530

    checking seperate import statements

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,7 @@
-import { cart, addToCart } from "../data/cart.js";
+import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+import { addToCart } from "../data/cart.js";
 
 let productsHTML = "";
 products.forEach((product) => {
